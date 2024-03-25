ada2f087f110b04188d46ef9d9a8a33f7ffec7c9
Find-Serendipity <find.serendipity@gmail.com>
2024-03-25 17:32:31 -0400

    second part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import {  } from ''
+import {puppyList} from './data.js'
+import { useState } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList)
 
   return (
     <>
-      
-      
+    {
+      puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>
+      })
+    }
     </>
   )
 }
